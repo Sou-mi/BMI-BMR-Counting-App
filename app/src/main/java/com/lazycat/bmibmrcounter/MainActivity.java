a07bda1e747309837f2f5503e76fdb98ad6548fd
soumi <subhadrasoumi@gmail.com>
2021-03-29 14:44:50 +0600

    BMI BMR Counter

--- app/src/main/java/com/lazycat/bmibmrcounter/MainActivity.java
+++ app/src/main/java/com/lazycat/bmibmrcounter/MainActivity.java
@@ -3,9 +3,15 @@ package com.lazycat.bmibmrcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private EditText weight;
+    private EditText height;
+    private Button male;
+    private Button female;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
